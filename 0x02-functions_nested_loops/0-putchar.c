12fe09f584528990295fc8b73ad446163bd22cdd
Covenant Monday <covenantmonday2018@.com>
2023-02-21 19:51:21 -0800

    task 0-16

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* main - a program prints _putchar, and by a new line
+* The putchar() function is used to write a
+* single character to the standard output stream,
+* it writes a single character to the standard output stream, stdout
+* Return: 0
+*/
+
+int main(void)
+{
+
+	 putchar('');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
